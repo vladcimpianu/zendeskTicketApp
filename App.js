bd6f71708ad7c370c87a070e94675f521c6062c3
Cimpianu Vlad <vlad.cimpianu@fortech.ro>
2020-08-05 23:26:07 +0300

    testing

--- App.js
+++ App.js
@@ -1,38 +1,32 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
-
-import ZendeskSupport from 'react-native-zendesk-support';
-
-const config = {
-  appId: 'c50167be8565fff4dcded1959f7c2a8195d01b968ff7caa3',
-  zendeskUrl: 'https://vlatech.zendesk.com',
-  clientId: 'mobile_sdk_client_207306d3c8c5c6de37b0',
-};
-ZendeskSupport.initialize(config);
-
-const identity = {
-  customerEmail: 'foo@bar.com',
-  customerName: 'John Doe',
-};
-ZendeskSupport.setupIdentity(identity);
-
-const customFields = {
-  customFieldId: 'Custom Field Value',
-};
-ZendeskSupport.callSupport(customFields);
-
-// Zendesk.INSTANCE.init(
-//   context,
-//   'https://vlatech.zendesk.com',
-//   'c50167be8565fff4dcded1959f7c2a8195d01b968ff7caa3',
-//   'mobile_sdk_client_207306d3c8c5c6de37b0',
-// );
-// Support.INSTANCE.init(Zendesk.INSTANCE);
+import {View, Text, TextInput, StyleSheet, Button} from 'react-native';
+import ZendeskSupport from '@synapsestudios/react-native-zendesk-support';
 
 const App = () => {
+  let sendTicket = () => {
+    alert('Ticket created');
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Zendesk Ticket Input</Text>
+      <View style={styles.textAreaContainer}>
+        <TextInput
+          className="name-input"
+          style={styles.input}
+          placeholder="Your name.."
+          placeholderTextColor="grey"
+        />
+        <TextInput
+          className="ticket-input"
+          style={styles.input}
+          placeholder="Your ticket text.."
+          placeholderTextColor="grey"
+          multiline={true}
+          numberOfLines={5}
+        />
+        <Button title="Submit" onPress={sendTicket} />
+      </View>
     </View>
   );
 };
@@ -40,12 +34,33 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
+    padding: 20,
+    marginTop: 100,
   },
   text: {
     color: 'darkslateblue',
     fontSize: 30,
+    margin: 30,
+  },
+  textAreaContainer: {
+    borderColor: 'darkslateblue',
+    justifyContent: 'flex-start',
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 5,
+    width: 250,
+    maxWidth: 250,
+  },
+  input: {
+    textAlign: 'center',
+    padding: 5,
+    margin: 20,
+    minWidth: 150,
+    borderBottomWidth: 1,
+    borderColor: 'black',
+    borderStyle: 'solid',
   },
 });
 
--- android/app/src/main/java/com/zendeskticketapp/MainApplication.java
+++ android/app/src/main/java/com/zendeskticketapp/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.robertsheao.RNZenDeskSupport.RNZenDeskSupport;
+import io.dcvz.rnzendesk.RNZendeskPackage;
 import io.dcvz.rnzendesk.RNZendeskPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
@@ -12,6 +14,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.robertsheao.RNZenDeskSupport.RNZenDeskSupport;
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -26,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
+          new RNZenDeskSupport()
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           return packages;
